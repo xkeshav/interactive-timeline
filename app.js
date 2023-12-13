6c146b3d3fc4eb2a9657c116ac319c9452556466
Keshav Mohta <xkeshav@gmail.com>
2023-12-14 00:08:36 +0530

    change in js file

--- app.js
+++ app.js
@@ -1,139 +1,151 @@
 // Set to store entered inputs
-const enteredInputs = new Set()
+const enteredInputs = new Set();
+
+const timeline = document.querySelector("#timeline");
+
+const userInput = document.querySelector("#inputField");
+
+// restrict to enter whitespace
+userInput.addEventListener("input", (e) => {
+  e.target.value = e.target.value.trim();
+});
+
+// handle paste event
+userInput.addEventListener("paste", (e) => {
+  console.log(e.target.value);
+  e.target.value = e.target.value.replace(/\s*/, "");
+});
 
 const handleInput = (e) => {
-    let enteredData = document.querySelector('#inputField').value
-    console.log(enteredData)
+  let enteredData = userInput.value;
+  console.log(enteredData);
 
-    //for removing whitespace
-    enteredData = enteredData.replace(/\s/g, '')
-    console.log('n', enteredData)
+  //for removing whitespace
+  enteredData = enteredData.replace(/\s/g, "");
+  console.log("n", enteredData);
 
-    //for checking alphanumeric characters
-    if (!/^[0-9a-zA-Z]+$/.test(enteredData)) {
-        showError('Please enter numbers or alphanumeric characters only.')
-        return
-    }
+  //for checking alphanumeric characters
+  if (!/^[0-9a-zA-Z]+$/.test(enteredData)) {
+    showError("Please enter numbers or alphanumeric characters only.");
+    return;
+  }
 
-    //checking maximum length
-    if (enteredData.length > 16) {
-        showError('Maximum characters allowed is 16')
-        return
-    }
+  //checking maximum length
+  if (enteredData.length > 16) {
+    showError("Maximum characters allowed is 16");
+    return;
+  }
 
-    //checking for duplicate entry
-    if (enteredInputs.has(enteredData.toLowerCase())) {
-        showError('Duplicate entry! This input already exists.')
-        return
-    }
+  //checking for duplicate entry
+  if (enteredInputs.has(enteredData.toLowerCase())) {
+    showError("Duplicate entry! This input already exists.");
+    return;
+  }
 
-    // checking for duplicate characters
-    // const duplicateNums = [...new Set(enteredData)]
-    // if ( duplicateNums.length !== enteredData.length) {
-    //     alert('Duplicate Characters are not allowed')
-    //     return
-    // }
+  // checking for duplicate characters
+  // const duplicateNums = [...new Set(enteredData)]
+  // if ( duplicateNums.length !== enteredData.length) {
+  //     alert('Duplicate Characters are not allowed')
+  //     return
+  // }
 
-    addToTimeline(enteredData)
-}
+  addToTimeline(enteredData);
+};
 
 const showError = (message) => {
-    const errorMsg = document.querySelector('.error')
-    errorMsg.textContent = message
-}
+  const errorMsg = document.querySelector(".error");
+  errorMsg.textContent = message;
+};
 
 const addToTimeline = (input) => {
-    const timeline = document.querySelector('#timeline')
-    const postDetails = new Date().toLocaleString()
-    const postElement = document.createElement('div')
-    postElement.classList.add('postContainer')
+  const postDetails = new Date().toLocaleString();
+  const postElement = document.createElement("div");
+  postElement.classList.add("postContainer");
 
-    // const containerDiv = document.createElement('div')
-    // containerDiv.classList.add('containerDiv')
+  // const containerDiv = document.createElement('div')
+  // containerDiv.classList.add('containerDiv')
 
-    const deleteBtn = document.createElement('button')
-    deleteBtn.classList.add('delete')
-    deleteBtn.innerHTML = '<i class="fa fa-trash-o" aria-hidden="true"></i>'
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("delete");
+  deleteBtn.innerHTML = "<span id='remove'>🗑️</span>";
 
-    const copyBtn = document.createElement('button')
-    copyBtn.classList.add('copy')
-    copyBtn.innerHTML = '<i class="fa fa-files-o" aria-hidden="true"></i>'
+  const copyBtn = document.createElement("button");
+  copyBtn.classList.add("copy");
+  copyBtn.innerHTML = '<i class="fa fa-files-o" aria-hidden="true"></i>';
 
-    //formatting text input to uppercase
-    const uppercaseFormat = input.charAt(0).toUpperCase() + input.slice(1)
+  //formatting text input to uppercase
+  const uppercaseFormat = input.charAt(0).toUpperCase() + input.slice(1);
 
-    // Create divs for input and date
-    const inputDiv = document.createElement('div');
-    inputDiv.classList.add('a');
-    inputDiv.textContent = uppercaseFormat;
+  // Create divs for input and date
+  const inputDiv = document.createElement("div");
+  inputDiv.classList.add("a");
+  inputDiv.textContent = uppercaseFormat;
 
-    const dateDiv = document.createElement('div');
-    dateDiv.classList.add('b');
-    dateDiv.innerHTML = `<div> ${postDetails} </div>`
+  const dateDiv = document.createElement("div");
+  dateDiv.classList.add("b");
+  dateDiv.innerHTML = `<div> ${postDetails} </div>`;
 
-    // container for usernames
-    const usernamesContainer = document.createElement('div');
-    usernamesContainer.classList.add('usernameContainer')
+  // container for usernames
+  const usernamesContainer = document.createElement("div");
+  usernamesContainer.classList.add("usernameContainer");
 
-    // Generating five random usernames
-    const randomUsername = generateRandomUsername();
-    const usernameDiv = document.createElement('div');
-    usernameDiv.classList.add('username');
-    usernameDiv.textContent = `${randomUsername}`;
-    // usernamesContainer.appendChild(usernameDiv);
-    dateDiv.appendChild(usernameDiv)
-    console.log('ab')
+  // Generating five random usernames
+  const randomUsername = generateRandomUsername();
+  const usernameDiv = document.createElement("div");
+  usernameDiv.classList.add("username");
+  usernameDiv.textContent = `${randomUsername}`;
+  // usernamesContainer.appendChild(usernameDiv);
+  dateDiv.appendChild(usernameDiv);
+  console.log("ab");
 
+  // Adding input, date, and usernames container to postElement
+  postElement.appendChild(inputDiv);
+  postElement.appendChild(dateDiv);
+  postElement.appendChild(deleteBtn);
+  postElement.appendChild(copyBtn);
+  // dateDiv.appendChild(usernamesContainer);
 
-    // Adding input, date, and usernames container to postElement
-    postElement.appendChild(inputDiv);
-    postElement.appendChild(dateDiv);
-    postElement.appendChild(deleteBtn);
-    postElement.appendChild(copyBtn);
-    // dateDiv.appendChild(usernamesContainer);
+  deleteBtn.addEventListener("click", (e) => {
+    timeline.removeChild(postElement);
+    alert(`${uppercaseFormat} successfully deleted!`);
+  });
 
-    deleteBtn.addEventListener('click', (e) => {
-        timeline.removeChild(postElement)
-        alert(`${uppercaseFormat} successfully deleted!`)
-    })
+  copyBtn.addEventListener("click", (e) => {
+    copyToClipboard(uppercaseFormat);
+    alert(`${uppercaseFormat} Successfully Copied to Clipboard`);
+  });
 
-    copyBtn.addEventListener('click', (e) => {
-        copyToClipboard(uppercaseFormat)
-        alert(`${uppercaseFormat} Successfully Copied to Clipboard`)
-    })
+  // add postElement to timeline
+  timeline.appendChild(postElement);
 
-    // add postElement to timeline
-    timeline.appendChild(postElement);
+  document.querySelector("#inputField").value = "";
 
-    document.querySelector('#inputField').value = ''
+  // Add input to the set of entered inputs
+  enteredInputs.add(input.toLowerCase());
 
-    // Add input to the set of entered inputs
-    enteredInputs.add(input.toLowerCase())
-
-    // Clear the error message
-    showError('');
-}
+  // Clear the error message
+  showError("");
+};
 
 const generateRandomUsername = () => {
-    const userNames = ['Developer', 'Designer', 'Contributor', 'Engineer', 'Trainee']
-    const randomIndex = Math.floor(Math.random() * userNames.length)
-    // console.log(randomIndex)
-    return userNames[randomIndex]
-}
+  const userNames = ["Developer", "Designer", "Contributor", "Engineer", "Trainee"];
+  const randomIndex = Math.floor(Math.random() * userNames.length);
+  // console.log(randomIndex)
+  return userNames[randomIndex];
+};
 
 const copyToClipboard = (text) => {
-    const textarea = document.createElement('textarea');
-    textarea.value = text;
-    document.body.appendChild(textarea);
-    textarea.select();
-    document.execCommand('copy');
-    document.body.removeChild(textarea);
-}
+  const textarea = document.createElement("textarea");
+  textarea.value = text;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand("copy");
+  document.body.removeChild(textarea);
+};
 
 //listening for the enter keypres in the inputField
-document.querySelector('#inputField').addEventListener('keypress', (e) => {
-    if (e.key === 'Enter') {
-        handleInput()
-    }
-})
-
+document.querySelector("#inputField").addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    handleInput();
+  }
+});
